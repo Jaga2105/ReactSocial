bf520e3c0dd410522f54f354c93c16db9792f7e2
Jagannath Nayak <jagannathnayak2105@gmail.com>
2024-02-06 09:03:41 +0530

    added  fetch header

--- client/src/api/userAPI.js
+++ client/src/api/userAPI.js
@@ -1,75 +1,90 @@
 const url = import.meta.env.VITE_API_URL;
 export const getUserDetails = async (id, token) => {
-    return await fetch(`${url}/user/${id}`, {
-      method: "GET",
-      headers: {
-        Authorization : `Bearer ${token}`
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => data)
-      .catch((error) => console.error("Error:", error));
-  };
+  return await fetch(`${url}/user/${id}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.error("Error:", error));
+};
 
-  export const searchUser = async (searchQuery) => {
-    return await fetch(`${url}/user/?username=${searchQuery}`, {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((data) => data)
-      .catch((error) => console.error("Error:", error));
-  };
+export const searchUser = async (searchQuery) => {
+  return await fetch(`${url}/user/?username=${searchQuery}`, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.error("Error:", error));
+};
 
-  export const updateUser = async (id, token, userData) => {
-    return await fetch(`${url}/user/${id}`, {
-      method: "PUT", 
-      headers: {
-        "Content-Type": "application/json",
-        Authorization : `Bearer ${token}`
-      },
-      body: JSON.stringify(userData),
-    })
-      .then((response) => response.json())
-      .then((data) => data)
-      .catch((error) => console.error("Error:", error));
-  };
+export const updateUser = async (id, token, userData) => {
+  return await fetch(`${url}/user/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.error("Error:", error));
+};
 
-  export const getSuggestedPeople = async (id, token) => {
-    return await fetch(`${url}/user/people/${id}`, {
-      method: "GET", 
-      headers: {
-        Authorization : `Bearer ${token}`
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => data)
-      .catch((error) => console.error("Error:", error));
-  };
+export const getSuggestedPeople = async (id, token) => {
+  return await fetch(`${url}/user/people/${id}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.error("Error:", error));
+};
 
-  export const followUser = async (id, token, followId) => {
-    return await fetch(`${url}/user/follow/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization : `Bearer ${token}`
-      },
-      body: JSON.stringify({followId}),
-    })
-      .then((response) => response.json())
-      .then((data) => data)
-      .catch((error) => console.error("Error:", error));
-  };
+export const followUser = async (id, token, followId) => {
+  return await fetch(`${url}/user/follow/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+    body: JSON.stringify({ followId }),
+  })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.error("Error:", error));
+};
 
-  export const unFollowUser = async (id, token, unFollowId) => {
-    return await fetch(`${url}/user/unfollow/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization : `Bearer ${token}`
-      },
-      body: JSON.stringify({unFollowId}),
-    })
-      .then((response) => response.json())
-      .then((data) => data)
-      .catch((error) => console.error("Error:", error));
-  };
+export const unFollowUser = async (id, token, unFollowId) => {
+  return await fetch(`${url}/user/unfollow/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+    body: JSON.stringify({ unFollowId }),
+  })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.error("Error:", error));
+};
